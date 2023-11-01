3cfbca810989b716c0569c5c62ace8119e51bb39
hd0167nutty <hd0167nutty@gmail.com>
2023-11-01 13:50:14 +0900

    4 commit

--- backend/main.py
+++ backend/main.py
@@ -8,13 +8,13 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.post("/auto", response_model=autos.AutoOut)
+@app.post("/auto/", response_model=autos.AutoOut)
 async def predict_auto(query: autos.AutoIn):
     """天気予報から明日の太陽光発電量予測"""
     return {'result':auto_solar.get_power_generations(query)}
     # return {'result': query.forecasts.minTemp}
 
-@app.get("/manual", response_model=manuals.ManualOut)
+@app.get("/manual/", response_model=manuals.ManualOut)
 async def predict_manual(query: manuals.ManualIn = Depends()):
     """入力値から明日の太陽光発電量予測"""
     return {'result':manual_solar.get_power_generations(query)}
